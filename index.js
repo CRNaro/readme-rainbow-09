cdaa62427cd30007ddf8f4a0a7dd18f1e6fae577
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-12-03 14:00:57 -0500

    Build: Added functionality to the prompt, with questions and template literal

--- index.js
+++ index.js
@@ -1,91 +1,134 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { type } = require('os');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
         name: 'description',
         message: 'What is the description of your project? Why did you build this project?  What problem does it solve? What did you learn? What did you learn from this project?',
-    };
+    },
     {
         type: 'confirm',
         name: 'tableOfContents',
         message: 'Would you like to include a table of contents?',
         default: true,
-    };
+    },
     {
         type: 'input',
         name: 'installation',
         message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
-    };
+    },
     {
         type: 'input',
         name: 'usage',
         message: 'Provide instructions and examples for use. Include screenshots as needed.',
-    };
+    },
     {
         type: 'input',
         name: 'credits',
         message: 'List your collaborators, if any, with links',
-    };
+    },
     {
         type: 'list',
         name: 'license',
         message: 'What kind of license should your project have?',
         choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
-    };
+    },
     {
         type: 'input',
         name: 'features',
         message: 'If your project has a lot of features, list them here.',
-    };
+    },
     {
         type: 'input',
         name: 'contributing',
         message: 'If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.',
-    };
+    },
     {
         type: 'input',
         name: 'tests',
         message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them.',
-    };
+    },
     {
         type: 'input',
-        name: 'questions',
+        name: 'githubUsername',
         message: 'Please enter your GitHub username.',
-    };
+    },
     {
         type: 'input',
-        name: 'questions',
+        name: 'email',
         message: 'Please enter your email address.',
-    };
+    },
     {
         type: 'input',
         name: 'features',
         message: 'If your project has a lot of features, list them here.',
-    };
+    },
     {
         type: 'input',
         name: 'tests',
         message: 'Any tests you have run for your application, please list them here.',
-    };
+    },
 ];
-.then (answers => {
-    const readmeContent = generateReadme(answers);
-    fs.writeFile('README.md', readmeContent);
-    console.log('README.md successfully created!');
-})
-.catch(error => {
-    console.error('Error generating README.md: ', error);
-});
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {}
-
+// TODO: Create a function to write README file
+function init() {
+    inquirer.prompt(questions)
+.then (answers => {
+    const readmeContent = generateMarkdown(answers);
+    fs.writeFile('README.md', readmeContent, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+})
+});
+}
 // Function call to initialize app
 init();
+
+//function writeToFile(fileName, data) {} - This was what was given to us in the starter code.  I'm not sure if I need to use this or not.  I'm not sure how to use it if I do need to use it.
+
+
+
+function writeReadme(answers){
+    return `
+    # ${answers.title}
+    
+    ## Description 
+    ${answers.description}
+
+    ## Table of Contents
+    -[Installation](#installation) 
+    -[Usage](#usage)
+    -[Credits](#credits)
+    -[License](#license)
+    -[Features](#features)
+    -[Tests](#tests)
+
+    ## Installation
+    ${answers.installation}
+
+    ## Usage
+    ${answers.usage}
+
+    ## Credits
+    ${answers.credits}
+
+    ## License
+    ${answers.license} license.
+
+    ## Features
+    ${answers.features || 'No features provided'}
+
+    ## Tests
+    ${answers.tests || 'No tests provided'}
+`;
+}
